a68bcb5d80e7b019102b42989daefc17a49bf3b4
Calvin Greguska <calvin.greguska@mymail.champlain.edu>
2021-04-19 21:19:33 -0400

    Initial commit

---
+++ Final Project/Final Project.cpp
@@ -0,0 +1,86 @@
+/*
+Author: Patricio Roji & Calvin Greguska
+Class : CSI - 240-01 & 240-02 : Advanced Programming
+Assignment : Final Project
+Date Assigned : ???
+Due Date : 3/27/21
+
+Description :
+Creates a program that makes a basic text based adventure that changes based on the
+player's input.
+
+Certification of Authenticity :
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the
+definition and consequences of plagiarism and acknowledge that the assessor
+of this assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+- Communicate a copy of this assignment to a plagiarism checking
+service (which may then retain a copy of this assignment on its
+database for the purpose of future plagiarism checking)
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Character {
+public:
+
+protected:
+};
+
+class Player : public Character {
+public :
+
+private:
+};
+
+class Enemy : public Character {
+public:
+
+private:
+};
+
+class NPC : public Character {
+public:
+
+private:
+};
+
+class Item {
+public:
+
+protected:
+};
+
+class HealthPacks : public Item {
+public:
+
+private:
+};
+
+class ManaPotions : public Item {
+public:
+
+private:
+};
+
+class Weapons : public Item {
+public:
+
+private:
+};
+
+class Armor : public Item {
+public:
+
+private:
+};
+
+int main()
+{
+    cout << "Nothing to see here :)\n";
+}
